a0ac45b4c8aca503ce7380a1104975fc27dd6c3f
mecaneer23 <mecaneer23@gmail.com>
2024-02-20 22:49:40 -0600

    feature: reformat document - make it work!

--- src/main.js
+++ src/main.js
@@ -1,3 +1,81 @@
-(async function run() {
-    console.log('Running async...');
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+function getRelativeTime(timestamp) {
+    const DAY_MILLISECONDS = 1000 * 60 * 60 * 24;
+    const rtf = new Intl.RelativeTimeFormat('en', {
+        numeric: 'auto',
+    });
+    const daysDifference = Math.round(
+        (timestamp - new Date().getTime()) / DAY_MILLISECONDS,
+    );
+
+    return rtf.format(daysDifference, 'day');
+}
+
+async function getMostRecentRepo(user) {
+    const url = `https://api.github.com/users/${user}/repos?per_page=100`;
+    return fetch(url)
+        .then(async response => {
+            data = await response.json();
+            if (!response.ok || data.length === 0) {
+                return [{ name: "repo_fetch_failed" }];
+            }
+            return data;
+        })
+        .then(data => {
+            data.sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at));
+            return data[0].name;
+        })
+        .catch(error => console.log('There was a problem with the fetch operation: ' + error.message));
+}
+
+async function getMostRecentCommit(user) {
+    const repo = await getMostRecentRepo(user)
+    const url = `https://api.github.com/repos/${user}/${repo}/commits`;
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                return [{
+                    commit: {
+                        message: "Fetching commits failed",
+                        author: {
+                            date: new Date().toISOString(),
+                        }
+                    },
+                    html_url: "https://mecaneer23.github.io/most-recent-git-commit",
+                    sha: "Try again"
+                }];
+            }
+            return response.json();
+        })
+        .then(data => {
+            return data[0].commit.author.date;
+        })
+        .catch(error => console.log('There was a problem with the fetch operation: ' + error.message));
+}
+
+(async () => {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASSWORD
+        }
+    });
+
+    const relativeTime = getRelativeTime(new Date(await getMostRecentCommit("mecaneer23")));
+
+    console.log("Sending email...");
+    await transporter.sendMail({
+        from: process.env.FROM,
+        to: process.env.TO,
+        subject: "Daily GitHub Commit Reminder",
+        html: `
+        <p>The last time you committed to GitHub was ${relativeTime}.</p>
+        <p>You should make a commit :)</p>
+    `,
+    });
 })();
